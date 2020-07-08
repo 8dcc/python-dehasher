c14eeaf3191fd45658b3ccbb05090c4da120557e
ravi0li <mikel.veg777@gmail.com>
2020-07-08 18:14:44 +0200

    v2.1
    Defined functions for colours and text styles with Colorama. Ready to use.

--- simple_dehasher.py
+++ simple_dehasher.py
@@ -3,9 +3,12 @@
 ###########################################################################
 #  Made by http://github.com/r4v10l1 for RaidForums.                      #
 #  https://raidforums.com/User-FakeRavi0li                                #
+#  Thanks to @Dark Lord, you are the best!                                #
 #  This script dehashes an MD5 hash using https://md5decrypt.net/en/Api/  #
 #  If it's not found, it bruteforces it with hashcat.                     #
 ###########################################################################
+
+# Try importing stuff
 try:
     import sys
     import os
@@ -17,12 +20,23 @@ except Exception:
     print()
     print(" [!] Error. Libraries not installed.")
     print(" [i] Run: pip install <package name>")
-    print(" [i] Required packages: sys, os, requests, bs4, colorama, time")
+    print(" [i] Required packages: sys, os, requests, bs4, colorama, time.")
     print()
     exit(1)
+#--------------------------------------------------------------------------------------------------
 
-#CHECK-ARGS-ARE-CORRECT----------------------------------------------------------------
+# Def the color types
+def info_text(text):
+    print(" %s%s[i] %s%s" % (Style.RESET_ALL, Fore.BLUE, text, Style.RESET_ALL))
+def success_text(text):
+    print(" %s%s%s[+] %s%s" % (Style.RESET_ALL, Style.BRIGHT, Fore.GREEN, text, Style.RESET_ALL))
+def error_text(text):
+    print(" %s%s%s[!] %s%s" % (Style.RESET_ALL, Style.BRIGHT, Fore.RED, text, Style.RESET_ALL))
+def input_text(text):
+    return input(" %s%s%s[*] %s >>%s " % (Style.RESET_ALL, Style.BRIGHT, Fore.BLUE, text, Fore.RESET))
+#--------------------------------------------------------------------------------------------------
 
+# Check if args are correct
 if len(sys.argv) == 1:
     print(" %s%s[-] Usagge: %s%s -e <target>" % (Style.BRIGHT, Fore.BLUE, Fore.RESET, sys.argv[0]))
     print("  %s│" % Fore.BLUE)
@@ -36,28 +50,28 @@ if len(sys.argv) == 1:
     exit(1)
 elif len(sys.argv) == 2:
     print()
-    print(" %s%s[!] Error. Not enough arguments.%s" % (Style.BRIGHT, Fore.RED, Fore.RESET))
+    error_text("Not enough arguments.")
     print()
     exit(1)
 elif len(sys.argv) == 3:
     if "-s" not in str(sys.argv[1]).strip():
         print()
-        print(" %s%s[!] Error. Type -s for single hash. Multiple hashes comming soon.%s" % (Style.BRIGHT, Fore.RED, Fore.RESET))
+        error_text("Error. Type -s for single hash. Multiple hashes comming soon.")
         print()
         exit(1)
     elif ".txt" in str(sys.argv[2]):
         print()
-        print(" %s%s[!] Error. Multiple hashes not supported in this version.%s" % (Style.BRIGHT, Fore.RED, Fore.RESET))
+        error_text("Error. Multiple hashes not supported in this version.")
         print()
         exit(1)
     elif len(str(sys.argv[2])) != 32:
         print()
-        print(" %s%s[!] Error. Invalid hash type. Only supported md5.%s" % (Style.BRIGHT, Fore.RED, Fore.RESET))
+        error_text("Error. Invalid hash type. Only supported md5.")
         print()
         exit(1)
 elif len(sys.argv) > 3:
     print()
-    print(" %s%s[!] Error. Too many arguments.%s" % (Style.BRIGHT, Fore.RED, Fore.RESET))
+    error_text("Error. Too many arguments.")
     print()
     exit(1)
 #--------------------------------------------------------------------------------------------------
@@ -93,19 +107,19 @@ sys.stdout.write('\r')
 
 # Email code
 try:
-    USER_EMAIL = input(" %s%s[*] Your email >> %s" % (Style.BRIGHT, Fore.BLUE, Fore.RESET))
+    USER_EMAIL = input_text("Your email")
     data = {'email_api':USER_EMAIL}
     try:
         r = requests.post("https://md5decrypt.net/en/Api/", data=data)
     except Exception:
-        print(" %s%s[!] Error. Request failed.%s" % (Style.BRIGHT, Fore.RED, Fore.RESET))
+        error_text("Error. Request failed.")
         print()
         exit(1)
     input(" %s%s[i] A verification code has been sent for the use of the API. %sPress Enter to continue..." % (Style.RESET_ALL, Fore.BLUE, Fore.RESET))
-    API_CODE = input(" %s%s[*] Your code >> %s" % (Style.BRIGHT, Fore.BLUE, Fore.RESET))
+    API_CODE = input_text("Your code")
 except KeyboardInterrupt:
     print()
-    print(" %s%s[!] Detected Ctrl+C. Shutting down...%s" % (Style.RESET_ALL, Fore.BLUE, Style.RESET_ALL))
+    error_text("Detected Ctrl+C. Shutting down...")
     exit(1)
 #--------------------------------------------------------------------------------------------------
 
@@ -125,26 +139,26 @@ if sys.argv[1] == "-s":
     PAGE_CONTENT = BeautifulSoup(PAGE.content, "html.parser").get_text()
     if "ERROR CODE : 001" in PAGE_CONTENT:
         print()
-        print(" %s%s[!] Error. Day limit exceded, running hashcat...%s" % (Style.RESET_ALL, Fore.RED, Style.RESET_ALL))
+        error_text("Error. Day limit exceded, running hashcat...")
         more_than_400()
         exit(1)
     elif "ERROR CODE : 002" in PAGE_CONTENT:
         print()
-        print(" %s%s[!] Error. Wrong email / code.%s" % (Style.RESET_ALL, Fore.REED, Style.RESET_ALL))
+        error_text("Error. Wrong email / code.")
         print()
         exit(1)
     elif "ERROR CODE : 005" in PAGE_CONTENT:
         print()
-        print(" %s%s[!] Error. Wrong hash tipe. Only MD5 supported.%s" % (Style.RESET_ALL, Fore.RED, Style.RESET_ALL))
+        error_text("Error. Wrong hash tipe. Only MD5 supported.")
         print()
         exit(1)
     if PAGE_CONTENT.strip() == "":
         print()
-        print(" %s%s[!] Hash not found.%s" % (Style.RESET_ALL, Fore.RED, Style.RESET_ALL))
+        error_text("Hash not found.")
         print()
         exit(1)
     else:
-        print(" %s%s[+] Success! Hash found.%s" % (Style.RESET_ALL, Fore.GREEN, Style.RESET_ALL))
+        success_text("Success! Hash found.")
         print(" " + HASH + Fore.RED +":" + Style.RESET_ALL + PAGE_CONTENT)
         exit(1)
 
